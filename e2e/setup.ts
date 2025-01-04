5e79877d58fa5c217cbfab095507da5d1036a096
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-01-04 12:47:56 +0200

    Load .env

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -3,11 +3,11 @@ import { config } from 'dotenv';
 import path from 'path';
 
 export function setupTestEnvs(): void {
-  config({ path: path.join(__dirname, '..', '.env.test') });
+  config({ path: path.join(__dirname, '..', '.env') });
 }
 
 export async function wipeTestDb(): Promise<void> {
   await axios.post(`${process.env.BACKEND_URL}/wipe-test-db`, {
-    adminKey: 'test',
+    adminKey: 'secret',
   });
 }
